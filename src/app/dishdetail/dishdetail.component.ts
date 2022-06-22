aa1e639022c1b8d403d5fb4bec545a2148c1ff00
Kseniya Kavalenka <kavalenkakseniya@gmail.com>
2022-06-22 23:49:39 +0200

    Assignment 3,Task 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -5,7 +5,7 @@ import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -21,6 +21,21 @@ export class DishdetailComponent implements OnInit {
   next: string;
   newComment: Comment;
   commentForm: FormGroup;
+
+  validationMessages = {
+    'author': {
+      'required':      'Name is required.',
+      'minlength':     'Name must be at least 2 characters long.'
+    },
+    'comment': {
+      'required':      'Comment is required.'
+    }
+  };
+
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
@@ -45,15 +60,38 @@ export class DishdetailComponent implements OnInit {
     this.location.back();
   }
 
-  createForm() {
+  createForm(): void {
     this.commentForm = this.fb.group({
       rating: 5,
-      comment: '',
-      author: '',
+      comment: ['', [Validators.required]],
+      author: ['', [Validators.required, Validators.minLength(2)]],
       date: ''
-    })
+    });
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
   }
   onSubmit() {
-    
+    console.log(this.commentForm.value);
+  }
+  onValueChanged(data?: any) {
+    if (!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 }
